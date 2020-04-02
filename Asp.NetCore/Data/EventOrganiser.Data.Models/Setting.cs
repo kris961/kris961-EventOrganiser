d1b888b9dd1081b853a19cc65ef876cb6094f2e3
kris961 <59437063+kris961@users.noreply.github.com>
2020-04-02 14:45:40 +0300

    Initial Commit

---
+++ Asp.NetCore/Data/EventOrganiser.Data.Models/Setting.cs
@@ -0,0 +1,11 @@
+﻿namespace EventOrganiser.Data.Models
+{
+    using EventOrganiser.Data.Common.Models;
+
+    public class Setting : BaseDeletableModel<int>
+    {
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
